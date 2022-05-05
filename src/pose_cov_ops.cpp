419b0969c0c891592addfb1ca4fba029ebb87689
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2022-05-05 13:32:10 +0200

    WIP

--- src/pose_cov_ops.cpp
+++ src/pose_cov_ops.cpp
@@ -1,15 +1,16 @@
 /*
  * pose_cov_ops.cpp
  *
- *  Created on: Mar 25, 2012
- *      Author: JLBC
- *
+ * License: BSD 3-Clause License
+ * Created on: Mar 25, 2012
+ * Author: JLBC
  */
 
 #include "pose_cov_ops/pose_cov_ops.h"
 
 #include <mrpt/poses/CPose3D.h>
 #include <mrpt/poses/CPose3DPDFGaussian.h>
+#include <mrpt/ros1bridge/pose.h>
 
 using namespace mrpt::poses;
 using namespace mrpt::math;
@@ -17,25 +18,22 @@ using namespace mrpt::math;
 void pose_cov_ops::compose(const geometry_msgs::Pose &a,
                            const geometry_msgs::Pose &b,
                            geometry_msgs::Pose &out) {
-  CPose3D A(UNINITIALIZED_POSE), B(UNINITIALIZED_POSE), OUT(UNINITIALIZED_POSE);
 
-  mrpt_bridge::convert(a, A);
-  mrpt_bridge::convert(b, B);
-
-  OUT.composeFrom(A, B);
-  mrpt_bridge::convert(OUT, out);
+  out = mrpt::ros1bridge::toROS_Pose(mrpt::ros1bridge::fromROS(a) +
+                                     mrpt::ros1bridge::fromROS(b));
 }
+
 void pose_cov_ops::compose(const geometry_msgs::PoseWithCovariance &a,
                            const geometry_msgs::PoseWithCovariance &b,
                            geometry_msgs::PoseWithCovariance &out) {
-  CPose3DPDFGaussian A(UNINITIALIZED_POSE), B(UNINITIALIZED_POSE);
 
-  mrpt_bridge::convert(a, A);
-  mrpt_bridge::convert(b, B);
+  const CPose3DPDFGaussian A = mrpt::ros1bridge::fromROS(a);
+  const CPose3DPDFGaussian B = mrpt::ros1bridge::fromROS(b);
 
   const CPose3DPDFGaussian OUT = A + B;
-  mrpt_bridge::convert(OUT, out);
+  out = mrpt::ros1bridge::toROS_Pose(OUT);
 }
+
 void pose_cov_ops::compose(const geometry_msgs::PoseWithCovariance &a,
                            const geometry_msgs::Pose &b,
                            geometry_msgs::PoseWithCovariance &out) {
